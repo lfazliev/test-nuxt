469a1e61c229d7a084706cb84968a206ac4a8949
listok195 <listok195@gmail.com>
2023-03-03 21:34:09 +0300

    first

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <nuxt-link to="/">home</nuxt-link>
+  <nuxt-link to="/about">about</nuxt-link>
+  <nuxt-page></nuxt-page>
 </template>
+<script>
+
+</script>
